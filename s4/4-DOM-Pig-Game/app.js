44861679323534decc54491cd29abe39b3209bea
Jessica Salmon <jessicasalmon05@gmail.com>
2016-12-22 19:32:17 +0000

    final code section creation

--- s4/4-DOM-Pig-Game/app.js
+++ s4/4-DOM-Pig-Game/app.js
@@ -115,12 +115,48 @@ document.querySelector('.dice').style.display = 'none';
 
     // 3. update the round score IF the rolled number was NOT a 1
 
-
-    // if (dice !== 0) {
-    //   roundScore += dice;
-    // }
-
   });
 
 
   // getElementByID - only works for ID, but faster than querySelector
+
+
+
+  /* *** 42. Lecture: Implementing Our 'Hold' Function and and the DRY Principle *** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  FINAL code
+var scores, roundScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+  var dice = Math.floor(Math.random() * 6) + 1;
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  document.querySelector('#current-' + activePlayer).textContent = dice;
+});
+*/
