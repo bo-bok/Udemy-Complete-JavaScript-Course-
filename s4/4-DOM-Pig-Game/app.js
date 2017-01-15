4ca9bf82eee79e68ff5b9d4149eb7b4c8ba20564
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-15 11:56:40 +0000

    correct mix up between pig game final and starter

---
+++ s4/4-DOM-Pig-Game/app.js
@@ -0,0 +1,28 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+
+// - how to create our fundemental game Variables
+
+  var scores, roundScore, activePlayer, dice;
+  scores = [0,0];
+  // this is cleaner than setting a score var for each player
+  roundScore = 0;
+  // there is only 1 round score at a time
+  activePlayer = 0;
+  // 0 will be 1st player and 1 will be 2nd player. 0 will read from first element in scores array, and 1 from the second
+
+
+// - how to generate a random number (for dice)
+  dice = 6;
+// - how to manipulate the DOM
+// - how to read from the DOM
+// - how to change CSS styles
